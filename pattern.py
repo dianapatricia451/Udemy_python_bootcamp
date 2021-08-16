85fe80a0ed8286fe66c3c90e486913028ad4e6c8
Carlos Espinosa <caiespin@ucsc.edu>
2021-08-15 21:16:57 -0700

    Fixing 0 at the begining

--- pattern.py
+++ pattern.py
@@ -7,7 +7,8 @@ for i in range(height+1):
        
     for j in range(i+1-i):
         
-        print(" *\t"*i,i,"\t","*\t"*i,end="")
+        if i != 0:
+            print(" *\t"*i,i,"\t","*\t"*i,end="")
       
 
     print("\r")
